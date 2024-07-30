3bcd937b3b14f55a0d4ff104722785101d2c702d
Sipioteo <matteo@sipio.it>
2024-07-31 01:50:27 +0200

    Workflow added

--- src/main.jsx
+++ src/main.jsx
@@ -20,6 +20,6 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-      <RouterProvider router={router} />
+      <RouterProvider router={router} basename='/mensa_app_addon/' />
   </React.StrictMode>,
 )
